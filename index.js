f517d94ae60b7126ffb335b007f08072e0e3f5ff
linaSlh <lyna.slh@gmail.com>
2023-11-23 15:59:12 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = 0;
+    for (let i = 0; i < this.length; i++) {
+      if (item > this.items[i]) {
+        index = i + 1;
+      } else {
+        break;
+      }
+    }
 
-  get(pos) {}
+    this.items.splice(index, 0, item);
 
-  max() {}
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos >= 0 && pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+  }}
+
+  max() { 
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+
+    let highest = this.items[0];
+    for (let i = 1; i < this.length; i++) {
+      if (highest < this.items[i]) {
+        highest = this.items[i];
+        return highest;
+      }
+    }
+
+  }
+
+  min() {
+    if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+
+  let lowest = this.items[0];
+  for (let i = 1; i < this.length; i++) {
+    if (lowest > this.items[i]) {
+      lowest = this.items[i];
+    }
+  }
+  return lowest;
+  }
 
   sum() {}
 
   avg() {}
 }
+const sortedList = new SortedList();
+sortedList.add(2);
+sortedList.add(5);
+sortedList.add(7);
+
+console.log(sortedList.min());
 
 module.exports = SortedList;
